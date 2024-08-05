1579621db66b6d7f69a4c170307aee34a69632ad
Shubham Gupta <11shubhamgupta99@gmail.com>
2024-08-05 15:46:09 +0530

    Debounce validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import { useState } from "react";
-import "./App.css";
+import { useContext, useEffect, useState } from 'react';
+import './App.css';
+import { ThemeContext } from './contexts/ThemeContext.js';
 
 function App() {
-  const [count, setCount] = useState(0);
-  return <>helo</>;
+  const [theme, toggleTheme] = useContext(ThemeContext);
+  const [name, setName] = useState('');
+  const [err, setErr] = useState('');
+  useEffect(() => {
+    if (name == '') {
+      setErr('');
+      return;
+    }
+    const id = setTimeout(() => {
+      console.log('checking name length...', name);
+      if (name.trim().length < 3) {
+        setErr('Name must be at least 3 characters long');
+      } else {
+        setErr('');
+      }
+    }, 1000);
+    () => clearTimeout(id);
+  }, [name]);
+  return (
+    <>
+      {theme}
+      <button onClick={() => toggleTheme()}>Change</button>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <p>{name}</p>
+      {err && <p>{err}</p>}
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ThemeContextProvider } from './components/ThemeContextProvider.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>
+);
