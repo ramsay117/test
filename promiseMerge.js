09355598943d853b8f09c2112818f83d8fb04e1e
11shubhamgupta99 <11shubhamgupta99@gmail.com>
2024-09-06 22:33:20 +0000

    Promise merge

---
+++ promiseMerge.js
@@ -0,0 +1,25 @@
+const promiseMerge = async (...promises) => {
+  try {
+    const result = await Promise.all(promises);
+    if (result.length == 0) throw new Error('No promises provided');
+    const firstType = Array.isArray(result[0]) ? 'array' : typeof result[0];
+    const isAllSameType = result.every(it => (Array.isArray(it) ? 'array' : typeof it) == firstType);
+    if (!isAllSameType) {
+      throw new TypeError('Types do not match')
+    }
+    if (firstType == 'number' || firstType == 'string') {
+      return result.reduce((a, b) => a + b);
+    }
+    if (firstType == 'boolean') {
+      return result.reduce((a, b) => a && b)
+    }
+    if (firstType == 'array') {
+      return result.reduce((a, b) => a.concat(b));
+    }
+    if (firstType == 'object') {
+      return result.reduce((a, b) => ({ ...a, ...b }))
+    }
+  } catch (error) {
+    throw error;
+  }
+};
