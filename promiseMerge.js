65fe950c3f329edcbb2e260008c34ea2ff7929d7
11shubhamgupta99 <11shubhamgupta99@gmail.com>
2024-09-07 20:05:17 +0000

    Add qs

--- promiseMerge.js
+++ promiseMerge.js
@@ -3,6 +3,9 @@ const promiseMerge = async (...promises) => {
     const result = await Promise.all(promises);
     if (result.length == 0) throw new Error('No promises provided');
     const firstType = Array.isArray(result[0]) ? 'array' : typeof result[0];
+    if(firstType == 'object' && Object.getPrototypeOf(result[0]) != Object.prototype) {
+      throw new TypeError('Invalid type');
+    }
     const isAllSameType = result.every(it => (Array.isArray(it) ? 'array' : typeof it) == firstType);
     if (!isAllSameType) {
       throw new TypeError('Types do not match')
@@ -23,3 +26,10 @@ const promiseMerge = async (...promises) => {
     throw error;
   }
 };
+
+async function promiseMergeResult() {
+  const result = await promiseMerge()
+  console.log(result);
+}
+
+promiseMergeResult();
